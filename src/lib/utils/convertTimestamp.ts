0abf3478e4cc751c5f12af395c5e1ab445264c83
Kaiser05gg <shu.iss1121@icloud.com>
2025-12-09 23:45:20 +0900

    importなどの調整

--- src/lib/utils/convertTimestamp.ts
+++ src/lib/utils/convertTimestamp.ts
@@ -1,17 +1,28 @@
-import { PollResult, VotedAt } from "features/poll/types/PollResult";
+import type {
+  DateLike,
+  PollDetailResult,
+} from "@/features/poll/types/PollResult";
 
-export const convertTimestamp = (t?: VotedAt): string => {
+export const convertTimestamp = (t?: DateLike): string => {
   if (!t) return "-";
-  const date =
-    typeof t === "string" ? new Date(t) : new Date(t._seconds * 1000);
+
+  let date: Date;
+
+  if (t instanceof Date) {
+    date = t;
+  } else if (typeof t === "string") {
+    date = new Date(t);
+  } else {
+    date = new Date(t._seconds * 1000);
+  }
+
   return date.toLocaleString("ja-JP", { timeZone: "Asia/Tokyo" });
 };
 
-// PollResult全体から最適な日時を抽出
-export const getPollDateText = (poll: PollResult): string => {
+export const getPollDateText = (poll: PollDetailResult): string => {
   return (
     convertTimestamp(poll.voted_at) ||
-    convertTimestamp(poll.created_at) ||
-    convertTimestamp(poll.updated_at)
+    convertTimestamp(poll.cron?.timestamp) ||
+    "-"
   );
 };
