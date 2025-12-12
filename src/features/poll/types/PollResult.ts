8d40881297a6d8ad712d78bf0a00624ce943ccbd
Kaiser05gg <shu.iss1121@icloud.com>
2025-12-12 13:31:01 +0900

    refactor:型PollDetailの追加

--- src/features/poll/types/PollResult.ts
+++ src/features/poll/types/PollResult.ts
@@ -3,14 +3,37 @@ export interface FirestoreTimestamp {
   _nanoseconds: number;
 }
 
-export type VotedAt = string | FirestoreTimestamp;
+export type DateLike = Date | string | FirestoreTimestamp | null;
 
-export interface PollResult {
-  date: string;
-  question: string;
-  results: Record<string, number>;
-  voted_at?: VotedAt;
-  created_at?: VotedAt;
-  updated_at?: VotedAt;
+export interface PollDetailResult {
+  // 親ドキュメント
+  id: string;
+  uuid: string;
+  question?: string;
+  created_at?: DateLike;
+  updated_at?: DateLike;
+  results?: Record<string, number> | null;
   top_option?: string;
+  voted_at?: DateLike;
+
+  cron?: {
+    results?: Record<string, number> | null;
+    top_option?: string;
+    timestamp?: DateLike;
+  } | null;
+
+  poll?: {
+    results?: Record<string, number> | null;
+    top_option?: string;
+    voted_at?: DateLike;
+  } | null;
+}
+export interface PollDetail {
+  id: string;
+  uuid: string;
+  question: string;
+  created_at?: string | FirestoreTimestamp;
+  top_option?: string;
+  results?: Record<string, number>;
+  voted_at?: string | FirestoreTimestamp;
 }
