ed5d8cb67163ad935908ae180bb415e3c84dd10e
Kaiser05gg <shu.iss1121@icloud.com>
2025-12-16 15:05:36 +0900

    fix(polls/page): PollTable に polls プロパティを渡し型エラーを解消

--- src/app/polls/page.tsx
+++ src/app/polls/page.tsx
@@ -1,5 +1,14 @@
-import Container from "./_components/Container";
+import PollListHeader from "./_components/PollListHeader";
+import { PollTable } from "@/features/poll/components/PollTable";
+import { fetchPollResults } from "@/features/poll/services/fetchPollResults";
 
-export default function PollListPage() {
-  return <Container />;
+export default async function Page() {
+  const polls = await fetchPollResults();
+
+  return (
+    <div className="max-w-3xl mx-auto mt-10">
+      <PollListHeader />
+      <PollTable polls={polls} />
+    </div>
+  );
 }
