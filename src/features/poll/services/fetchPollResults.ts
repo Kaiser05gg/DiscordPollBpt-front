356338434bb859eadad6264c2e805398b3c3a1a3
Kaiser05gg <shu.iss1121@icloud.com>
2025-12-16 14:25:38 +0900

    fix:urlの変更

--- src/features/poll/services/fetchPollResults.ts
+++ src/features/poll/services/fetchPollResults.ts
@@ -1,10 +1,13 @@
 import { PollDetailResult } from "../types/PollResult";
 
 export const fetchPollResults = async (): Promise<PollDetailResult[]> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/poll_results`
-  );
-  console.log("CALL:", `${process.env.NEXT_PUBLIC_API_BASE_URL}/poll_results`);
+  const base = process.env.NEXT_PUBLIC_API_BASE_URL!;
+  const url = `${base}/api/poll_results`;
+
+  console.log("CALL:", url);
+
+  const res = await fetch(url, { cache: "no-store" });
+
   if (!res.ok) throw new Error(`API Error: ${res.status}`);
   return await res.json();
 };
