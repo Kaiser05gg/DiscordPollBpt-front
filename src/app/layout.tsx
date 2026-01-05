ab9bbe8c1ec91a590ff98d77f4da48b50d637d34
Kaiser05gg <shu.iss1121@icloud.com>
2026-01-05 13:31:08 +0900

    add:classサイドバーの追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Sidebar } from "@/app/_components/Sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex min-h-screen">
+          {/* サイドバー */}
+          <Sidebar />
+
+          {/* メインコンテンツ */}
+          <main className="flex-1 bg-black-100 p-6">{children}</main>
+        </div>
       </body>
     </html>
   );
